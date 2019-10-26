08c8848ca838b5f2ac4864a0b30e0367d7298bb0
evanparizot <eparizot@gmail.com>
2019-10-26 01:27:13 -0400

    Initial about page/component build out

--- src/app/features/home/projects/projects.data.ts
+++ src/app/features/home/projects/projects.data.ts
@@ -18,12 +18,34 @@ export const projects: Project [] = [
     sourceUrl: "https://github.com/evanparizot/catan"
   },
   {
+    title: "Cutting Board",
+    description: "3d cutting board using cherry, walnut, and maple",
+    backgroundUrl: "http://assets.parizot.info/evan/projects/3DcuttingBoard.jpg",
+    source: "Imgur",
+    sourceUrl: "https://imgur.com/a/9ZtARZi"
+  },
+  {
     title: "Wordclock",
     description: "PCB and code for prototype wordclock",
     backgroundUrl: "",
     source: "Github",
     sourceIcon: "github",
     sourceUrl: "https://github.com/evanparizot/wordclock"
+  },
+  {
+    title: "Toolboxes",
+    description: "Japanese style toolboxes made out of walnut, maple, and cherry",
+    backgroundUrl: "http://assets.parizot.info/evan/projects/japanese_toolbox.jpg",
+    source: "Imgur",
+    sourceUrl: "https://imgur.com/a/yiolIla"
+  },
+  {
+    title: "Topographic Map",
+    description: "Topographic map of Austin, Texas using public GIS information",
+    backgroundUrl: "",
+    source: "github",
+    sourceIcon: "github",
+    sourceUrl: "https://github.com/evanparizot/austin-map"
   },
   {
     title: "Website",
@@ -40,5 +62,13 @@ export const projects: Project [] = [
     source: "Imgur",
     sourceIcon: "",
     sourceUrl: "https://imgur.com/a/PeQ0E"
+  },
+  {
+    title: "JBL Speakers",
+    description: "Restored vintage JBL L26 Century Speakers",
+    backgroundUrl: "",
+    source: "Imgur",
+    sourceIcon: "",
+    sourceUrl: "https://imgur.com/a/pdVpFP3"
   }
 ]
