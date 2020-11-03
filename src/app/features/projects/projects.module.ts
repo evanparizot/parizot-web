25d20298cf7e3047ebdd6dc68dc58d1f8761c8f7
Evan Parizot <54151852+evanparizot@users.noreply.github.com>
2020-11-02 22:36:16 -0500

    Project view (#32)
    * Generate project component
    
    * Update dependencies

--- src/app/features/projects/projects.module.ts
+++ src/app/features/projects/projects.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectComponent } from './project/project.component';
 
 const routes: Routes = [
   {
@@ -14,7 +15,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [ProjectsComponent],
+  declarations: [ProjectsComponent, ProjectComponent],
   imports: [
     CommonModule,
     SharedModule,
