4cb8f3a3ec97cf1fe70bcbe64706aa5aba3d5dbf
Evan <eparizot@gmail.com>
2021-06-05 00:24:29 -0400

    Get ng build working after angular update. Comment out unused sass

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,6 +1,5 @@
-import { ModuleWithProviders, NgModule} from "@angular/core";
-import { MAT_LABEL_GLOBAL_OPTIONS, MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
-import { MatIconRegistry } from '@angular/material/icon';
+import { NgModule} from "@angular/core";
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
